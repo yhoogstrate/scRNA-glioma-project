e4c1ef0411a48ecee001552405e8e12b49b14b84
yhoogstrate <y.hoogstrate@gmail.com>
2022-12-02 14:09:20 +0100

    update

--- annotate_van_Hijfte_dataset.py
+++ annotate_van_Hijfte_dataset.py
@@ -32,54 +32,90 @@ import scanpy as sc
 wd = "data/Glimmunology_GBM_1/Glioma_Y_and_O/Levi2_Glioma_Y/outs/raw_feature_bc_matrix"
 
 
-adata = sc.read_10x_mtx(
+adata_raw = sc.read_10x_mtx(
     wd,  # the directory with the `.mtx` file
     var_names='gene_symbols',                # use gene symbols for the variable names (variables-axis index)
     cache=True)   
 
+adata = adata_raw
+
+# adata.var_names_make_unique()
+
+    
+
 #%%
-
-adata.var_names_make_unique()
-
 
 sc.pl.highest_expr_genes(adata, n_top=20, )
 
+#%%  -- filter low res
 
 sc.pp.filter_cells(adata, min_genes=200)
 sc.pp.filter_genes(adata, min_cells=3)
 
-adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
-sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+#%%
+# annotate the group of mitochondrial genes as 'mt'
 
-# The small it's set, the more it excludes?
-#adata = adata[adata.obs.n_genes_by_counts < 1500, :]
-#adata = adata[adata.obs.pct_counts_mt < 4, :]
-adata = adata[adata.obs.n_genes_by_counts > 1500, ]
-adata = adata[adata.obs.pct_counts_mt < 4,]
+adata.var['mt'] = adata.var_names.str.startswith('MT-')
+sum(adata.var_names.str.startswith('MT-'))
 
 
-sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'],
-             jitter=0.4, multi_panel=True)
+#%%
+
+sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], 
+                           percent_top=None, 
+                           log1p=False, 
+                           inplace=True)
+
+#%% 
 
 sc.pl.scatter(adata, x='total_counts', y='pct_counts_mt')
 sc.pl.scatter(adata, x='total_counts', y='n_genes_by_counts')
 
+sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'],
+             jitter=0.4, multi_panel=True)
 
+#%%
+
+
+# The small it's set, the more it excludes?
+adata = adata[adata.obs.n_genes_by_counts > 1250, ]
+adata = adata[adata.obs.total_counts > 1250, ]
+adata = adata[adata.obs.pct_counts_mt < 4,]
+
+min(adata.obs.total_counts)
+sum(adata.obs.total_counts == 0)
+
+
+#%%
 
 sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'],
              jitter=0.4, multi_panel=True)
 
+#%%
+
 
 # Total-count normalize (library-size correct) the data matrix X to 10,000 reads per cell, so that counts become comparable among cells.
 sc.pp.normalize_total(adata, target_sum=1e4)
+sc.pp.log1p(adata)
 
+
+
+#%%
 
 sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
 sc.pl.highly_variable_genes(adata)
 
 
-sc.pp.filter_cells(adata, min_genes=500)
-sc.pp.filter_genes(adata, min_cells=5)
+# %% 
+
+adata = adata[:, adata.var.highly_variable]
+print("n[high var] = %d" % sum(adata.var.highly_variable))
+
+#%%
+
+
+sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt'])
+sc.pp.scale(adata, max_value=10)
 
 
 
